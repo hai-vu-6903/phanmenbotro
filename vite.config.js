88a068d0f94427d7e327166fbcffc9717fd89f6f
Vũ Thế Hải <167839872+hai-vu-6903@users.noreply.github.com>
2025-11-25 10:02:36 +0700

    delete vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [
     react(),
     VitePWA({
@@ -20,55 +19,26 @@ export default defineConfig({
         background_color: "#1a2e1a",
         icons: [
           {
-            src: "./src/assets/img/pwa-192.png",
+            src: "img/icons/icon-192x192.png",
             sizes: "192x192",
-            type: "image/png",
+            type: "image/png"
           },
           {
-            src: "./src/assets/img/pwa-512.png",
+            src: "img/icons/icon-512x512.png",
             sizes: "512x512",
-            type: "image/png",
+            type: "image/png"
           },
           {
-            src: "./src/assets/img/maskable.png",
+            src: "img/icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "maskable",
-          },
-        ],
+            purpose: "any maskable"
+          }
+        ]
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
       },
     }), 
   ],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        entryFileNames: 'assets/js/[name].[hash].js',
-        chunkFileNames: 'assets/js/[name].[hash].js',
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const ext = info[info.length - 1];
-          if (/png|jpe?g|svg|gif|webp|avif|ico/i.test(ext)) {
-            return 'assets/images/[name].[hash][extname]';
-          }
-          if (/mp3|wav|ogg|m4a/i.test(ext)) {
-            return 'assets/audio/[name].[hash][extname]';
-          }
-          if (/mp4|webm|ogv/i.test(ext)) {
-            return 'assets/video/[name].[hash][extname]';
-          }
-          if (/pdf|docx?|xlsx?/i.test(ext)) {
-            return 'assets/documents/[name].[hash][extname]';
-          }
-          return 'assets/[name].[hash][extname]';
-        }
-      }
-    }
-  }
 });
